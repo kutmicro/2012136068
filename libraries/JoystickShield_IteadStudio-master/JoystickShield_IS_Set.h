1852576f61fced5732525008dfd9592d052384c1
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-10 12:51:11 +0900

    Add files via upload

--- libraries/JoystickShield_IteadStudio-master/JoystickShield_IS_Set.h
+++ libraries/JoystickShield_IteadStudio-master/JoystickShield_IS_Set.h
@@ -1,30 +1,31 @@
-#include "JoystickShield_IS.h"
+#include "JoystickShield_Joy.is.h"
 int gear;
+JoystickShield Joy;
 int Joystick_XY(){
 	int dir=0;		//방향을 나타낸다.
-	if(isCenter()==true) dir=0;
-	else if(isUp()==true) dir=8;
-	else if(isDown()==true)dir=2;
-	else if(isLeft()==true)dir=4;
-	else if(isRight()==true)dir=6;
-	else if(isLeftUp()==true)dir=7;
-	else if(isLeftDown()==true)dir=1;
-	else if(isRigthUp()==true)dir=9;
-	else if(isRightDown()==true)dir=3;
+	if(Joy.isCenter()==true) dir=0;
+	else if(Joy.isUp()==true) dir=8;
+	else if(Joy.isDown()==true)dir=2;
+	else if(Joy.isLeft()==true)dir=4;
+	else if(Joy.isRight()==true)dir=6;
+	else if(Joy.isLeftUp()==true)dir=7;
+	else if(Joy.isLeftDown()==true)dir=1;
+	else if(Joy.isRigthUp()==true)dir=9;
+	else if(Joy.isRightDown()==true)dir=3;
 	else return 0;
-
+	delay(20);
 	return dir;
 }
 
 int Joystick_Button(){
 	int but=0;
-	if(isJoystickButoon()==true) but=1;
-	else if(isUpButton()==true) but=2;
-	else if(isDownButton()==true)but=3;
-	else if(isLeftButton()==true)but=4;
-	else if(isRightButton()==true)but=5;
-	else if(isFButton()==true)but=6;
-	else if(isEButton()==true)but=7;
+	if(Joy.isJoystickButoon()==true) but=1;
+	else if(Joy.isUpButton()==true) but=2;
+	else if(Joy.isDownButton()==true)but=3;
+	else if(Joy.isLeftButton()==true)but=4;
+	else if(Joy.isRightButton()==true)but=5;
+	else if(Joy.isFButton()==true)but=6;
+	else if(Joy.isEButton()==true)but=7;
 	else but=0;
 
 	return but;
