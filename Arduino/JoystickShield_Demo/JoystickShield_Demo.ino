1c464b663fb7733c7bf12fa85f89d4537be9fc3b
SongSeungha <Song>
2016-12-13 10:45:17 +0900

    Final!

--- Arduino/JoystickShield_Demo/JoystickShield_Demo.ino
+++ Arduino/JoystickShield_Demo/JoystickShield_Demo.ino
@@ -1,22 +1,35 @@
 #include"JoystickShield_IS_set.h"
+ int val=0;
 SoftwareSerial BT(14,15);
 void setup(){
   Serial.begin(9600);
   BT.begin(9600);
+ 
 }
 
 void loop(){
-
-  Joy.processCallbacks();
+  val=(val/10);
+  val=val*10;
+  /*Joy.processCallbacks();
   int dir;
   dir = Joystick_XY();
   int but;
   but = Joystick_Button();
   int sum=dir*10+but;
-  BT.print(dir);
+  BT.write(sum);*/
+  /*
+  Serial.print("x:");
+  Serial.println(analogRead(1));
+  Serial.print("y:");
+  Serial.println(analogRead(0));
+*/
+  if(Serial.available())
+   {
+    val=Serial.parseInt();
+    BT.write(val);
   
-  Serial.print(sum);
- // Serial.print(but);
+   }
+  Serial.print(val);
   delay(100);
   Joy.clear();
 }
